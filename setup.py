88daee51069256d1793c2451dd4e7f2004257aaa
Kais Ben Salah <kaisbe@protonmail.com>
2018-11-01 13:13:47 +0100

    credits and package updates

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 setup(
-    name = "test",
+    name = "pyharu",
+    version = '0.2',
+    description = 'Python bindings for libhpdf (aka Haru PDF C library)',
+    author = 'Kais Ben Salah',
+    url = 'https://github.com/kaisbe/pyharu'
     cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension('pyharu', ['pyharu/pyharu.pyx'], libraries=['hpdf', 'z', 'png'])], # accepts a glob pattern
 )
